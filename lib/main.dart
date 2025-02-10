99fa0827b707758a4f1b87d311a743fd1f524da3
Sayed Sinan Umer Jifry <sayedsinan7@gmail.com>
2025-02-10 08:39:19 -0800

    Button added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,27 @@
-void main(){
-   
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:git_tracker/view/landing/splash_page.dart';
+
+void main() async {
+  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      initialRoute: '/splash',
+      getPages: [
+        GetPage(
+          name: '/splash',
+          page: () => const SplashPage(),
+        ),
+      ],
+    );
+  }
 }
---
+++ lib/view/landing/splash_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:git_tracker/view/widgets/my_button.dart';
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+    
+    );
+  }
+}
